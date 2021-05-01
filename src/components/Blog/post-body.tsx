03ec5e015b04241066f3bc85e4f14c3432acbf4a
Matthieu Riegler <kyro38@gmail.com>
2021-05-02 01:57:52 +0200

    bump react-markdown to 6.0

--- src/components/Blog/post-body.tsx
+++ src/components/Blog/post-body.tsx
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import markdownStyles from './markdown-styles.module.css'
 
-const renderers: { [nodeType: string]: ElementType } = {
+const components: { [nodeType: string]: ElementType } = {
   image: function image(image: HTMLImageElement) {
     const imgSrc = require(`../../../public/${image.src}?sizes[]=300,sizes[]=600`);
     return <img srcSet={imgSrc.srcSet} alt={image.alt} height="200" width="355" /> as JSX.Element
@@ -13,7 +13,7 @@ const renderers: { [nodeType: string]: ElementType } = {
 export default function PostBody({ content }: { content: string }): JSX.Element {
   return (
     <div className="max-w-2xl mx-auto">
-      <ReactMarkdown plugins={[remarkGfm]} className={markdownStyles["markdown"]} renderers={renderers} >
+      <ReactMarkdown plugins={[remarkGfm]} className={markdownStyles["markdown"]} components={components} >
         {content}
       </ReactMarkdown>
     </div>
