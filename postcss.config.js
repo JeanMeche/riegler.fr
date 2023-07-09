638011d2e98f512dc8d1825204f1e29bb256f7cd
Matthieu Riegler <kyro38@gmail.com>
2023-07-09 16:35:18 +0200

    initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
 module.exports = {
-  plugins: [
-    'postcss-nesting',
-    'postcss-calc',
-    'postcss-advanced-variables',
-    'postcss-import-url',
-    'postcss-nth-list',
-    'postcss-color-function',
-    'postcss-url',
-    'tailwindcss',
-    'autoprefixer',
-  ],
-}
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
