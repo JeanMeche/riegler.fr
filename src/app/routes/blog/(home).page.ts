19e6423b46b2d6678b94e5ae462c782926a17cdd
Matthieu Riegler <kyro38@gmail.com>
2023-10-04 01:15:19 +0200

    feat(blog): filter out future articles from homepage/blog page

--- src/app/routes/blog/(home).page.ts
+++ src/app/routes/blog/(home).page.ts
@@ -1,10 +1,10 @@
-import { injectContentFiles } from '@analogjs/content';
-import { AsyncPipe, NgForOf } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { ContentMetadata } from '../../../lib/content-metadata/content-metadata';
-import { BlogPreviewComponent } from '../../components/blog/blog-preview/blog-preview.component';
-import { PageHeaderComponent } from '../../components/layout/page-header/page-header.component';
+import {injectContentFiles} from '@analogjs/content';
+import {AsyncPipe, NgForOf} from '@angular/common';
+import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {ContentMetadata} from '../../../lib/content-metadata/content-metadata';
+import {BlogPreviewComponent} from '../../components/blog/blog-preview/blog-preview.component';
+import {PageHeaderComponent} from '../../components/layout/page-header/page-header.component';
 
 @Component({
   selector: 'blog',
@@ -18,22 +18,20 @@ import { PageHeaderComponent } from '../../components/layout/page-header/page-he
     PageHeaderComponent,
   ],
   template: `
-    <app-page-header
-      title="Blog"
-      intro="Writing about stuff."
-    ></app-page-header>
+    <app-page-header title="Blog" intro="Writing about stuff."></app-page-header>
     <div class="mt-12 flex max-w-3xl flex-col space-y-16">
-      <app-blog-preview
-        [article]="article"
-        *ngFor="let article of blogArticles"
-      ></app-blog-preview>
+      <app-blog-preview [article]="article" *ngFor="let article of blogArticles"></app-blog-preview>
     </div>
   `,
 })
 export default class BlogComponent {
   public blogArticles = [
     ...injectContentFiles<ContentMetadata>((contentFile) =>
-      contentFile.filename.includes('/src/content/posts')
+      contentFile.filename.includes('/src/content/posts'),
     ),
-  ].sort((a1, a2) => (a1.attributes.date > a2.attributes.date ? -1 : 1));
+  ]
+    .filter((article) => {
+      return new Date() > new Date(article.attributes.date);
+    })
+    .sort((a1, a2) => (a1.attributes.date > a2.attributes.date ? -1 : 1));
 }
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,11 @@
-import { injectContentFiles } from '@analogjs/content';
-import { AsyncPipe, NgForOf } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { ContentMetadata } from '../../lib/content-metadata/content-metadata';
-import { FeaturedBlogPreviewComponent } from '../components/blog/featured-blog-preview/featured-blog-preview.component';
-import { PageHeaderComponent } from '../components/layout/page-header/page-header.component';
-import { StravaComponent } from '../components/strava/strava.component';
+import {injectContentFiles} from '@analogjs/content';
+import {AsyncPipe, NgForOf} from '@angular/common';
+import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {ContentMetadata} from '../../lib/content-metadata/content-metadata';
+import {FeaturedBlogPreviewComponent} from '../components/blog/featured-blog-preview/featured-blog-preview.component';
+import {PageHeaderComponent} from '../components/layout/page-header/page-header.component';
+import {StravaComponent} from '../components/strava/strava.component';
 
 @Component({
   selector: 'home',
@@ -33,10 +33,7 @@ import { StravaComponent } from '../components/strava/strava.component';
     </h2>
 
     <div class="mt-6 w-full grid grid-cols-1 gap-4">
-      <app-featured-blog-preview
-        [article]="article"
-        *ngFor="let article of blogArticles"
-      />
+      <app-featured-blog-preview [article]="article" *ngFor="let article of blogArticles" />
     </div>
     <div class="flex flex-col items-end	p-4">
       <a
@@ -48,8 +45,8 @@ import { StravaComponent } from '../components/strava/strava.component';
           class="absolute -inset-y-4 -inset-x-2 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-900/50 sm:-inset-x-6 sm:rounded-2xl"
         ></span>
 
-        <span class="z-10"
-          >See more
+        <span class="z-10">
+          See more
           <svg
             viewBox="0 0 16 16"
             fill="none"
@@ -77,8 +74,11 @@ import { StravaComponent } from '../components/strava/strava.component';
 })
 export default class HomeComponent {
   public blogArticles = injectContentFiles<ContentMetadata>((contentFile) =>
-    contentFile.filename.includes('/src/content/posts')
+    contentFile.filename.includes('/src/content/posts'),
   )
+    .filter((article) => {
+      return new Date() > new Date(article.attributes.date);
+    })
     .sort((a1, a2) => (a1.attributes.date > a2.attributes.date ? -1 : 1))
     .slice(0, 4);
 }
