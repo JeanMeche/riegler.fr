9e1761f701a9049e87ae4aa58ff7fbb64b235033
Matthieu Riegler <kyro38@gmail.com>
2023-10-07 23:40:39 +0200

    blog: @defer part 2.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
+import 'zone.js';
+
 import {provideContent, withMarkdownRenderer} from '@analogjs/content';
 import {provideFileRouter} from '@analogjs/router';
 import {bootstrapApplication} from '@angular/platform-browser';
-import 'zone.js';
+import {withInMemoryScrolling, withViewTransitions} from '@angular/router';
 
-import {withInMemoryScrolling} from '@angular/router';
 import {AppComponent} from './app/app.component';
+import 'prismjs/components/prism-cshtml';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideFileRouter(
       withInMemoryScrolling({anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled'}),
+      withViewTransitions(),
     ),
     provideContent(withMarkdownRenderer()),
   ],
