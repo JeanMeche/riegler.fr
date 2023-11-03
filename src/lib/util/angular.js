0693a7d4edc89c73d17ecdf960b5364443ae75d8
Matthieu Riegler <kyro38@gmail.com>
2023-11-03 22:31:54 +0100

    fix: improve prism regex for highlighting

--- src/lib/util/angular.js
+++ src/lib/util/angular.js
@@ -1,12 +1,43 @@
 (function (Prism) {
   Prism.languages.angular = Prism.languages.extend('markup', {
-    'keyword': /(?:@if|@for|@switch|@defer|@loading|@error|@placeholder|prefetch)\b/,
-    'operator': /\b(?:on|when)\b/,
-    'number': {
-      pattern: /\b(minimum|after)\s+\d+(?:s|ms|)/gi,
+    'deferEtc': {
+      pattern:
+        // matches the content of the outer most parenthesis after defer/placeholder/loading
+        /@(defer|placeholder|loading)\s*\(?[^)(]*(?:\([^)(]*(?:\([^)(]*(?:\([^)(]*\)[^)(]*)*\)[^)(]*)*\)[^)(]*)*\)?/gi,
+      inside: {
+        'keyword': /(?:@defer|@placeholder|@loading|prefetch)/,
+        'operator': /\b(when|on)\b/,
+        'number': {
+          pattern: /\b(minimum\s|after\s|\()\s*\d+(?:s|ms|)/gi,
+          lookbehind: true,
+        },
+        'builtin': {
+          pattern:
+            /(?:viewport|timer|minimum|after|hover|idle|immediate|interaction|\$index|\$count|\$odd|\$even|\$first|\$last)/,
+        },
+
+        'function':
+          /#?(?!\s)[_$a-zA-Z\xA0-\uFFFF](?:(?!\s)[$\w\xA0-\uFFFF])*(?=\s*(?:\.\s*(?:apply|bind|call)\s*)?\()/,
+      },
+    },
+    'ctrlFlow': {
+      pattern:
+        // matches the content of the outer most parenthesis after defer/placeholder/loading
+        /@(if|for|switch)\s*\([^)(]*(?:\([^)(]*(?:\([^)(]*(?:\([^)(]*\)[^)(]*)*\)[^)(]*)*\)[^)(]*)*\)/gi,
+      inside: {
+        'keyword': /(?:@for|@if|@switch|@case|track|let)/,
+        'operator': /\b(of)\b/,
+        'function':
+          /#?(?!\s)[_$a-zA-Z\xA0-\uFFFF](?:(?!\s)[$\w\xA0-\uFFFF])*(?=\s*(?:\.\s*(?:apply|bind|call)\s*)?\()/,
+        'builtin': {pattern: /\$index|\$count|\$odd|\$even|\$first|\$last/},
+      },
+    },
+
+    'keyword': {
+      pattern: /(?:@if|@for|@switch|@loading|@error|@placeholder)\b/,
       lookbehind: true,
     },
-    'builtin': {pattern: /\b(?:viewport|timer|minimum|after|hover|idle|immediate|interaction)/},
+    'operator': /\b\(.*(?:on|when|of|track|let).*\)\b/,
     'function':
       /#?(?!\s)[_$a-zA-Z\xA0-\uFFFF](?:(?!\s)[$\w\xA0-\uFFFF])*(?=\s*(?:\.\s*(?:apply|bind|call)\s*)?\()/,
   });
